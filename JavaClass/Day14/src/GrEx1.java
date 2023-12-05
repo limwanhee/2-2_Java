675b9ab04ca618ceae02c0976ccdca51fabbb60a
unknown <applespeed550@gmail.com>
2023-12-05 15:46:42 +0900

    14주 계산기, 퍼즐, 텍스트 필드 예제, 그래픽, JMenu

--- JavaClass/Day14/src/GrEx1.java
+++ JavaClass/Day14/src/GrEx1.java
@@ -10,7 +10,7 @@ public class GrEx1 extends JFrame{
 		MyPanel pan = new MyPanel();
 		add("Center", pan);
 		
-		setSize(300, 200);
+		setSize(400, 330);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
@@ -18,7 +18,14 @@ public class GrEx1 extends JFrame{
 		@Override
 		public void paint(Graphics g) {
 			super.paint(g);
+			//출력할 문자열, x, y
 			g.drawString("drawString()을 이용한 문자열 출력", 50, 100);
+			//x, y 시작위치, x, y의 끝 위치 
+			g.drawLine(50, 30, 250, 150);
+			g.drawLine(50, 120, 270, 60);
+			//x, y 좌표, w 넓이, h 높이
+			g.drawRect(50, 120, 300, 120);
+			g.drawRect(80, 40, 140, 220);
 		}
 	}
 	public static void main(String[] args) {
